69a97a9641ba4199ef8d87da4fd8e4c001cc669d
Jekaterina Kackan <soulnyshko@gmail.com>
2017-07-05 15:22:51 +0300

    first commit

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
+app.get('/1', function (req, res) {
   res.render('home');
+})
+
+app.get('/2', function (req, res) {
+    res.render('home2');
 })
 
 app.listen(3000, function () {
